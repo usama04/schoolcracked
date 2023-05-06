0d0c0c489314efeb576eed7d3edc9f074fe9daf8
usama04 <usama.uoa@gmail.com>
2023-05-06 17:17:51 +0800

    Started Adding theme for landing page

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,23 +1,68 @@
-import React from 'react'
-import '../styles/navstyle.css'
-import { Link } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { UserContext } from '../context/UserContext'
+import { useNavigate, Link } from 'react-router-dom'
+//import '../styles/navstyle.css'
+import '../fonts/icomoon/style.css'
+import '../fonts/brand/style.css'
+import '../styles/landing/css/bootstrap.min.css'
+import '../styles/landing/css/bootstrap-datepicker.css'
+import '../styles/landing/css/jquery.fancybox.min.css'
+import '../styles/landing/css/owl.carousel.min.css'
+import '../styles/landing/css/owl.theme.default.min.css'
+import '../styles/landing/css/aos.css'
+import '../styles/landing/css/style.css'
 
 const Nav = () => {
-  return (
-    <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-  <div className="container">
-    <a className="navbar-brand" href="/"><img src="logo.png" alt="logo" width="30" height="30" className="d-inline-block align-text-top" /></a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarCollapse">
-      <ul className="navbar-nav me-auto mb-2 mb-md-0">
+  const { setToken } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const logout = () => {
+    setToken(null);
+    navigate('/');
+  }
+
+  const navbar_menu_authenticated = (
+    <nav className="site-navigation text-right ml-auto d-none d-lg-block" role="navigation">
+      <ul className="site-menu main-menu js-clone-nav ml-auto ">
+        <li><Link to="/chat" className="nav-link">Chat TA</Link></li>
+        <li><button className='nav-link' onClick={logout}>Logout</button></li>
       </ul>
-      <button className="btn btn-outline-success me-2"><Link to="/login" className='text-white nav-link'>Login</Link></button>
-      <button className="btn btn-outline-success"><Link to="/register" className='text-white nav-link'>Register</Link></button>
-    </div>
-  </div>
-</nav>
+    </nav>
+  )
+
+  const navbar_menu_unauthenticated = (
+    <nav className="site-navigation text-right ml-auto d-none d-lg-block" role="navigation">
+      <ul className="site-menu main-menu js-clone-nav ml-auto ">
+        <li><Link to="index.html" className="nav-link">Home</Link></li>
+        <li><Link to="about.html" className="nav-link">About</Link></li>
+        <li><Link to="contact.html" className="nav-link">Contact</Link></li>
+        <li><Link to="/login" className="nav-link btn btn-primary px-4 py-2 rounded-0"><span className="icon-users"></span> Login</Link></li>
+        <li><Link to="/register" className="nav-link btn btn-primary px-4 py-2 rounded-0"><span className="icon-users"></span> Register</Link></li>
+      </ul>
+    </nav>
+  )
+
+  return (
+    <header className="site-navbar light site-navbar-target" role="banner">
+      <div className="container">
+        <div className="row align-items-center position-relative">
+
+          <div className="col-3">
+            <div className="site-logo">
+              <Link to="/"><img src="logo-white.png" alt="logo" height="200" className="d-inline-block align-text-top" /></Link>
+            </div>
+          </div>
+
+          <div className="col-9  text-right">
+
+            <span className="d-inline-block d-lg-none"><Link to="#" className=" site-menu-toggle js-menu-toggle py-5 "><span className="icon-menu h3 text-black"></span></Link></span>
+
+            {localStorage.getItem('token') > 5 ? navbar_menu_authenticated : navbar_menu_unauthenticated}
+
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,46 +1,116 @@
 import React, { useEffect } from 'react'
 import { Nav } from '../components/Nav'
 import { useNavigate, Link } from 'react-router-dom'
-import '../styles/landing.css'
+import '../fonts/icomoon/style.css'
+import '../fonts/brand/style.css'
+import '../styles/landing/css/bootstrap.min.css'
+import '../styles/landing/css/bootstrap-datepicker.css'
+import '../styles/landing/css/jquery.fancybox.min.css'
+import '../styles/landing/css/owl.carousel.min.css'
+import '../styles/landing/css/owl.theme.default.min.css'
+import '../styles/landing/css/aos.css'
+import '../styles/landing/css/style.css'
+
 
 const Landing = () => {
   const navigate = useNavigate()
 
   useEffect(() => {
+    //loadScripts(scripts)
     try {
       const token = localStorage.getItem('usertoken')
       if (token.length > 5) {
         navigate('/chat')
       }
     } catch (error) {
-      
+
     }
   }, [navigate])
 
   return (
-    <div className='App '>
-      <div className='section1'>
-      <div className="container">
+    <div className='App'>
+      <div className="site-wrap" id="home-section">
+
+        <div className="site-mobile-menu site-navbar-target">
+          <div className="site-mobile-menu-header">
+            <div className="site-mobile-menu-close mt-3">
+              <span className="icon-close2 js-menu-toggle"></span>
+            </div>
+          </div>
+          <div className="site-mobile-menu-body"></div>
+        </div>
+
+
+
         <Nav />
-        <div className="p-5 rounded-lg m-3">
-          <section className="py-5 text-center container">
-            <div className="row py-lg-5">
-              <div className="col-lg-8 col-md-8 mx-auto">
-                <h1 className="fw-light"><img src="logo.png" alt="AalimBot" width="200" /></h1>
-                <p className="lead text-white fs-3">Your AI Islamic Scholar for Quick Answers on Quran and Sharia. Get instant responses to your questions, based on Quranic and Hadith references. AalimBot is the perfect companion for anyone seeking accessible knowledge on Islamic teachings.</p>
-                <p>
-                  {/*<Link to="https://forms.gle/uzhDvh95wQCEdyJd7" className='text-white nav-link'><button type="button" className="btn btn-success btn-lg my-2">Join our Waitlist</button></Link>*/}
-                  <Link to="/register" className='text-white nav-link'><button type="button" className="btn btn-success btn-lg my-2">Register your account</button></Link>
-                </p>
-                <br />
-                <p className="alert alert-warning">Please note that AalimBot is still a work in progress and we are only in Beta testing. Its responses may not always be fully accurate or authentic. Use the information provided by AalimBot at your own discretion and always verify it with other reliable sources.</p>
+
+        <div className="site-section-cover overlay" style={{ backgroundImage: 'url("Carousel.png")' }}>
+
+          <div className="container">
+            <div className="row align-items-center justify-content-center">
+              <div className="col-lg-10 text-center">
+                <h1>The <strong>Hub</strong> Of <strong>Tutorials</strong></h1>
               </div>
             </div>
-          </section>
+          </div>
         </div>
-      </div>
+
+        <div className="site-section bg-light pb-0">
+          <div className="container">
+            <div className="row align-items-stretch overlap">
+              <div className="col-lg-8">
+                <div className="box h-100">
+                  <div className="d-flex align-items-center">
+                    {/* <div className="img"><img src="images/img_1.jpg" className="img-fluid" alt="Image" /></div> */}
+                    <div className="text">
+                      <Link to="#" className="category">Tutorial</Link>
+                      <h3><Link to="#">Learning React Native</Link></h3>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum quidem totam exercitationem eveniet blanditiis nulla, et possimus, itaque alias maxime!</p>
+                      <p className="mb-0">
+                        <span className="brand-react h5"></span>
+                        <span className="brand-javascript h5"></span>
+                      </p>
+                      <p className="meta">
+                        <span className="mr-2 mb-2">1hr 24m</span>
+                        <span className="mr-2 mb-2">Advanced</span>
+                        <span className="mr-2 mb-2">Jun 18, 2020</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="box small h-100">
+                  <div className="d-flex align-items-center mb-2">
+                    {/* <div className="img"><img src="images/img_2.jpg" className="img-fluid" alt="Image" /></div> */}
+                    <div className="text">
+                      <Link to="#" className="category">Tutorial</Link>
+                      <h3><Link to="#">Learning React Native</Link></h3>
+                    </div>
+                  </div>
+                  <div className="d-flex align-items-center mb-2">
+                    {/* <div className="img"><img src="images/img_3.jpg" className="img-fluid" alt="Image" /></div> */}
+                    <div className="text">
+                      <Link to="#" className="category">Tutorial</Link>
+                      <h3><Link to="#">Learning React Native</Link></h3>
+                    </div>
+                  </div>
+                  <div className="d-flex align-items-center">
+                    {/* <div className="img"><img src="images/img_4.jpg" className="img-fluid" alt="Image" /></div> */}
+                    <div className="text">
+                      <Link to="#" className="category">Tutorial</Link>
+                      <h3><Link to="#">Learning React Native</Link></h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
+
   )
 }
 
