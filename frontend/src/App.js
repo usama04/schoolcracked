daf45fa32fca89bedef0ec32915377ab07042772
usama04 <usama.uoa@gmail.com>
2023-05-11 23:59:53 +0800

    Created Login system using react redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,14 @@ import { Chat } from './pages/Chat';
 import { UserContext } from './context/UserContext';
 import { Routes, Route } from 'react-router-dom';
 import { Landing } from './pages/Landing';
-import { Login } from './pages/Login';
+import Login from './pages/Login';
 import { Register } from './pages/Register';
 import { ResetPassword } from './pages/ResetPassword';
 import { ForgotPassword } from './pages/ForgotPassword';
 import { VerifyEmail } from './components/VerifyEmail';
+import Activate from './pages/Activate';
+import { Provider } from 'react-redux';
+import Store from './components/Store';
 //import { AltAnswer } from './components/AltAnswer';
 
 
@@ -19,16 +22,18 @@ function App() {
 
   return (
     <>
+    <Provider store={Store}>
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       {token && <Route path="/chat" element={<Chat />} /> }
       <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/reset-password/:token" element={<ResetPassword />} />
-      <Route path="/verify-email/:token" element={<VerifyEmail />} />
+      <Route path="/password/reset/confirm/:uid/:token" element={<ResetPassword />} />
+      <Route path="/activate/:uid/:token" element={<Activate />} />
       <Route path="*" element={<Landing />} />
     </Routes>
+    </Provider>
     </>
     
   );
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,63 +1,60 @@
-import React, { useContext, useState } from 'react';
-import { UserContext } from '../context/UserContext';
+import React, { useState } from 'react';
 import '../styles/sign-in.css';
 import { ErrorMessage } from '../components/ErrorMessage';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { Nav } from '../components/Nav';
+import { login } from '../actions/auth';
 
 
-const Login = () => {
+const Login = ({ login }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const { setToken } = useContext(UserContext);
     const [errorMessages, setErrorMessages] = useState([]);
     const navigate = useNavigate();
 
-    const submitLogin = async () => {
-        try {
-            const requestOptions = {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                body: JSON.stringify(`grant_type=&username=${email}&password=${password}&scope=&client_id=&client_secret=`),
-            };
-            const response = await fetch(`${process.env.REACT_APP_API_URL}/api/login`, requestOptions);
-            const data = await response.json();
-            if (data.access_token) {
-                setToken(data.access_token);
-                navigate('/chat');
-            } else {
-                setErrorMessages("Invalid username or password");
-            }
-        } catch (error) {
-            setErrorMessages("Invalid username or password");
-        }
-    }
-
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        submitLogin();
+        login(email, password)
     }
+    // Is the user logged in?
+    // If so, redirect them to the chat page
 
     return (
         <div className="App">
             <Nav />
-            <main className="form-signin w-100 m-auto mt-5 pt-5">
-                {errorMessages.length > 0 && <ErrorMessage message={errorMessages} />}
-                <form onSubmit={handleSubmit}>
-                    <h1 className="h3 mb-3 fw-normal">Please Login</h1>
-                    <div className="form-group">
-                        <label htmlFor="username" className="control-label">Email</label>
-                        <input type="email" className="form-control rounded-2" id="username" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control rounded-2" id="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} />
-                    </div>
-                    <button className="w-100 btn btn-lg btn-primary" type="submit">Login</button>
-                    <a href="/forgot-password" className="mt-3">Forgot Password?</a>
-                </form>
-            </main>
+            <div className="container mt-5 pt-5">
+                <main className="form-signin w-100 m-auto mt-5 pt-5">
+                    {errorMessages.length > 0 && <ErrorMessage message={errorMessages} />}
+                    <form onSubmit={handleSubmit}>
+                        <h1 className="h3 mb-3 fw-normal">Please Login</h1>
+                        <div className="form-group">
+                            <label htmlFor="username" className="control-label">Email</label>
+                            <input type="email" className="form-control rounded-2" id="username" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" className="form-control rounded-2" id="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} />
+                        </div>
+                        <button className="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+                        <Link to="/forgot-password" className="mt-3">Forgot Password?</Link>
+                    </form>
+                    <hr />
+                    <p>Don't have an account?</p>
+                    <Link to="/register" className="w-100 btn btn-lg btn-success">Register</Link>
+                </main>
+            </div>
         </div>
     )
 }
-export { Login }
+
+// const mapStateToProps = (state) => {
+//     // is_authenticated: state.auth.is_authenticated,
+//     // user: state.auth.user,
+//     // error: state.auth.error,
+//     // loading: state.auth.loading,
+//     // token: state.auth.token,
+//     // refresh: state.auth.refresh,
+// }
+
+export default connect(null, { login })(Login);
