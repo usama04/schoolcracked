a8fecc0038cd3c86e9448c09b510678684924712
usama04 <usama.uoa@gmail.com>
2023-05-11 12:02:56 +0800

    New CSS files added to frontend, Serializers added to AuthBackend

--- AuthBackend/accounts/models.py
+++ AuthBackend/accounts/models.py
@@ -1,10 +1,4 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
+
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
@@ -46,7 +40,7 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     objects = UserAccountManager()
     
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = [first_name]
+    REQUIRED_FIELDS = ['first_name']
     
     def __repr__(self):
         return f"User: {self.first_name} {self.last_name}, Email: {self.email}"
---
+++ AuthBackend/accounts/serializers.py
@@ -0,0 +1,13 @@
+from djoser.serializers import UserCreateSerializer
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+User = get_user_model()
+
+class UserCreateSerializer(UserCreateSerializer):
+    password = serializers.CharField(write_only=True)
+    
+    class Meta(UserCreateSerializer.Meta):
+        model = User
+        fields = ("id", "email", "first_name", "last_name", "company_name", "location", "profile_pic_url", "birth_date", "password", "is_active", "is_staff")
